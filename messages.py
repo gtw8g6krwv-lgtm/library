893bb5ecafd71d0c2fbfb8121ad2ac38d7baf251
daraborisenko <dashaborisenko2002@mail.ru>
2026-03-30 22:07:37 +1000

    Добавлен класс Messages

---
+++ messages.py
@@ -0,0 +1,7 @@
+class Messages:
+    BOOK_NOT_FOUND = "Ошибка! Книга не найдена."
+    BOOK_ALREADY_LOANED = "Книга '{}' уже выдана."
+    BOOK_LOANED = "Книга '{}' выдана на руки!"
+    BOOK_ALREADY_AVAILABLE = "Книга '{}' и так в библиотеке."
+    BOOK_RETURNED = "Книга '{}' возвращена!"
+    BOOK_ADDED = "Книга добавлена! ID: {}"
