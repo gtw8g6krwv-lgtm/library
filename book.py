001829a3a3f9198b2c1a3a45a1c826ed47aae146
daraborisenko <dashaborisenko2002@mail.ru>
2026-04-09 18:44:39 +1000

    Поля сделаны приватными

--- book.py
+++ book.py
@@ -1,8 +1,8 @@
 class Book:
     def __init__(self, book_id: str, title: str, author: str, year: int):
-        self.book_id = book_id
-        self.title = title.strip()
-        self.author = author.strip()
-        self.year = year
-        self.is_available = True
-        self.current_holder = None
+        self.__book_id = book_id
+        self.__title = title.strip()
+        self.__author = author.strip()
+        self.__year = year
+        self.__is_available = True
+        self.__current_holder = None
